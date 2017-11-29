c01c58e07afbc37874b9dbb39582214bac732ad3
Cody March <cody.march84@gmail.com>
2017-11-29 08:48:38 -0600

    added user input

--- capLetterFunction.js
+++ capLetterFunction.js
@@ -1,7 +1,10 @@
-function capLetter(){
-  let txt = prompt("Type in a word");
-  let txt= "Hello";
+function capWords(word1, word2){
+  return word1 + " " + word2;
+}
+function getUserInput(){
+  let txt= prompt("Type in a word (lower case)");
   return txt;
 }
-let txt= capLetter();
-console.log(txt);
+let word1=getUserInput();
+let word2=getUserInput();
+console.log(capWords(word1, word2));

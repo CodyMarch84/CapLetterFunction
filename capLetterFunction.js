7f397e53f884f6d5ce02ee79218779dc912fd5a7
Cody March <cody.march84@gmail.com>
2017-11-30 07:15:18 -0600

    added 'use strict'

--- capLetterFunction.js
+++ capLetterFunction.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function capWords(word1){
 //Seperate words in a string based on spaces
   let splitWords = word1.split(" ");
@@ -22,6 +24,30 @@ function getUserInput(){
   // let txt= capWords(input);
   //
   // console.log(txt);
+
+  // function capitalWords(splitWords){
+  //     let capArray = [];
+  // for (let i = 0;i < splitWords.length; i++){
+  //     let word =  splitWords[i];
+  //     let capWord = word[0].toUpperCase() + word.slice(1);
+  //     capArray.push(capWord);
+  //   }
+  //   return capArray;
+  // }
+  // function capWords(word1){
+  // //Seperate words in a string based on spaces
+  //   let splitWords = word1.split(" ");
+  //   let capArray = capitalWords(splitWords);
+  //   let result = capArray.join(" ");
+  //   return result;
+  // }
+  //
+  // function getUserInput(){
+  //   let input = prompt();
+  //   return input;
+  // }
+  //
+  //   console.log(capWords(getUserInput()));
 
  //functionExample("Hello", "World");
 //function joined or concatenates words together
